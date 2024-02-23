29068e83dfd0d41f0448bcf60cfbc4af363207c0
Sebastian Richner <sebastian@richner.ag>
2024-02-23 10:28:16 +0100

    Clean up, fix typos

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,7 +5,7 @@ import determineActivity from "./determineActivity";
 
 /**
  * This is a cross-platform tracker class that allows you to subscribe to active window changes. It does so by wrapping the 'active-win' library found at: https://www.npmjs.com/package/active-win
- * It should be noted that per default in case a window was active for less than 1 second, there is a possbility that the callback will not fire. If you need to have more precise window change events, consider lowering "checkingForWindowChangeInterval"
+ * It should be noted that per default in case a window was active for less than 1 second, there is a possibility that the callback will not fire. If you need to have more precise window change events, consider lowering "checkingForWindowChangeInterval"
  */
 export class WindowsActivityTracker implements ITracker {
   name = "WindowsActivityTracker";
@@ -21,7 +21,7 @@ export class WindowsActivityTracker implements ITracker {
 
   /**
    * Constructor for creating a WindowsActivityTracker instance
-   * @param onWindowChange This is a callaback function that receives the activeWindow as an argument and is fired whenever the active window changes.
+   * @param onWindowChange This is a callback function that receives the activeWindow as an argument and is fired whenever the active window changes.
    * @param checkingForWindowChangeInterval The interval that is used to check for active window changes (in milliseconds)
    * @param accessibilityPermission From active-win: Enable the accessibility permission check. Setting this to false will prevent the accessibility permission prompt on macOS versions 10.15 and newer. The url property won't be retrieved.
    * @param screenRecordingPermission From active-win: Enable the screen recording permission check. Setting this to false will prevent the screen recording permission prompt on macOS versions 10.15 and newer. The title property in the result will always be set to an empty string.
@@ -66,7 +66,7 @@ export class WindowsActivityTracker implements ITracker {
           (this._prev.windowTitle !== window.windowTitle ||
             this._prev.process !== window.process)
         ) {
-          // for performance reasons we only determine the activity once we actually have to
+          // For performance reasons we only determine the activity once we actually have to
           const activity = determineActivity(res?.title, res?.owner.name);
           const activeWindow: ActiveWindow = { ...window, activity };
           this.onWindowChange(activeWindow);
