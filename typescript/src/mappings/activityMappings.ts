4cd54c02252f1cdf91e50bafcb2559432e55923d
André Meyer <info@andre-meyer.ch>
2026-04-07 19:52:15 +0200

    [#28] fix: add .js extensions to imports for node16 moduleResolution compatibility

--- typescript/src/mappings/activityMappings.ts
+++ typescript/src/mappings/activityMappings.ts
@@ -1,18 +1,18 @@
-import Activity from "../types/Activity";
-import * as devCode from "./devCode";
-import * as devDebug from "./devDebug";
-import * as devReview from "./devReview";
-import * as devVc from "./devVc";
-import * as email from "./email";
-import * as planning from "./planning";
-import * as readWriteDocument from "./readWriteDocument";
-import * as design from "./design";
-import * as socialMedia from "./socialMedia";
-import * as generativeAI from "./generativeAI";
-import * as instantMessaging from "./instantMessaging";
-import * as fileManagement from "./fileManagement";
-import * as other from "./other";
-import * as otherRdp from "./otherRdp";
+import Activity from "../types/Activity.js";
+import * as devCode from "./devCode.js";
+import * as devDebug from "./devDebug.js";
+import * as devReview from "./devReview.js";
+import * as devVc from "./devVc.js";
+import * as email from "./email.js";
+import * as planning from "./planning.js";
+import * as readWriteDocument from "./readWriteDocument.js";
+import * as design from "./design.js";
+import * as socialMedia from "./socialMedia.js";
+import * as generativeAI from "./generativeAI.js";
+import * as instantMessaging from "./instantMessaging.js";
+import * as fileManagement from "./fileManagement.js";
+import * as other from "./other.js";
+import * as otherRdp from "./otherRdp.js";
 
 const activityMappings = {
   [Activity.DevCode]: devCode,
